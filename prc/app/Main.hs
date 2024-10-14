8569c549acce910e8f784b1021f8c88880d6a8aa
Kwanghoon Choi <lazyswamp@gmail.com>
2024-10-14 18:32:02 +0900

    continuation

--- prc/app/Main.hs
+++ prc/app/Main.hs
@@ -1,11 +1,18 @@
 module Main (main) where
 
 import Example
-import Interp
+import Interp(valueOf)
+import InterpK(valueOfProgramK)
 import Env
 
 main :: IO ()
-main = do let v = valueOf ex1_2 EmptyEnv client2
+main = do let v = valueOfProgramK ex1_2 client2
           print ex1_2
           putStrLn ""
           print v
+
+
+          let w = valueOf ex1_2 EmptyEnv client2
+          print ex1_2
+          putStrLn ""
+          print w
